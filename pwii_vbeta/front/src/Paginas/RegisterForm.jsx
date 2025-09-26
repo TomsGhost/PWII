1b583a9f0c0103fdc5a96eb9eba0df8154b31ec4
TomsGhost <167227045+TomsGhost@users.noreply.github.com>
2025-09-25 18:10:15 -0600

    Update

--- pwii_vbeta/front/src/Paginas/RegisterForm.jsx
+++ pwii_vbeta/front/src/Paginas/RegisterForm.jsx
@@ -1,119 +1,134 @@
-import React, { useState } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
-import './styleRegister.css'; 
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import "./styleRegister.css";
+import axios from "axios";
+
 const RegisterForm = () => {
-    const squares = [0, 1, 2, 3, 4];
-    const [fullname, setFullname] = useState('');
-    const [email, setEmail] = useState('');
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
-    const [confirmPassword, setConfirmPassword] = useState('');
-    const navigate = useNavigate();
+  const squares = [0, 1, 2, 3, 4];
+  const [fullname, setFullname] = useState("");
+  const [email, setEmail] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [archivo, setArchivo] = useState(null);
+  const navigate = useNavigate();
 
-    const handleSubmit = async (e) => {
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
-        e.preventDefault(); 
+    const frmData = new FormData();
+    frmData.append("name", fullname);
+    frmData.append("aka", username);
+    frmData.append("mail", email);
+    frmData.append("pass", password);
+    frmData.append("file", archivo);
 
-        try {
-            //backend
-            const response = await fetch('http://localhost:3001/api/register', { 
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({ email, username, password }), 
-            });
+    try {
+      const respuesta = await axios.post(
+        "http://localhost:3001/register",
+        frmData,
+        { headers: { "Content-Type": "multipart/form-data" } }
+      );
 
-            const data = await response.json();
+      if (respuesta.data.msg === "REGISTRO EXITOSO") {
+        alert("Usuario registrado");
+      } else if (respuesta.data.msg !== null) {
+        alert("No es posible registar al usuario: " + respuesta.data.msg);
+      } else {
+        alert("Error al registar usuario");
+      }
+      console.log(respuesta.data);
+    } catch (error) {
+      console.log(error);
+      alert("Error en la peticion");
+    }
+  };
 
-            if (response.ok) {
-                alert('¡Registro exitoso!');
-                navigate('/'); 
-            } else {
-                alert(`Error: ${data.message}`); 
-            }
-        } catch (error) {
-            console.error('Error de red:', error);
-            alert('No se pudo conectar al servidor.');
-        }
-    };
+  return (
+    <section>
+      <div className="color"></div>
+      <div className="color"></div>
+      <div className="color"></div>
+      <div className="box">
+        {squares.map((i) => (
+          <div key={i} className="square" style={{ "--i": i }}></div>
+        ))}
 
-    return (
-         <section>
-            <div className="color"></div>
-            <div className="color"></div>
-            <div className="color"></div>
-            <div className="box">
-                {squares.map((i) => (
-                    <div key={i} className="square" style={{ '--i': i }}></div>
-                ))}
-                
-                <div className="container">
-                    <div className="form">
-                        <h2>Register</h2>
-                        <form onSubmit={handleSubmit}> 
-                            <div className="inputBox">
-                                <input 
-                                    type="text" 
-                                    name="fullname" 
-                                    placeholder="Nombre" 
-                                    required 
-                                    value={fullname}
-                                    onChange={(e) => setFullname(e.target.value)}
-                                />
-                            </div>
-                            <div className="inputBox">
-                                <input 
-                                    type="email" 
-                                    name="email" 
-                                    placeholder="Email" 
-                                    required 
-                                    value={email}
-                                    onChange={(e) => setEmail(e.target.value)}
-                                />
-                            </div>
-                            <div className="inputBox">
-                                <input 
-                                    type="text" 
-                                    name="username" 
-                                    placeholder="Username" 
-                                    required 
-                                    value={username} 
-                                    onChange={(e) => setUsername(e.target.value)}
-                                />
-                            </div>
-                            <div className="inputBox">
-                                <input 
-                                    type="password" 
-                                    name="password" 
-                                    placeholder="Password" 
-                                    required 
-                                    value={password}
-                                    onChange={(e) => setPassword(e.target.value)}
-                                />
-                            </div>
-                            <div className="inputBox">
-                                <input 
-                                    type="password" 
-                                    name="confirmPassword" 
-                                    placeholder="Confirm Password" 
-                                    required
-                                    value={confirmPassword}
-                                    onChange={(e) => setConfirmPassword(e.target.value)}
-                                />
-                            </div>
-                            <div className="inputBox">
-                                <input type="submit" value="Registrarse" />
-                            </div>
-                            <p className="forget">
-                                Already have an account? <Link to="/"> Login</Link>
-                            </p>
-                        </form>
-                    </div>
-                </div>
-            </div>
-        </section>
-    );
+        <div className="container">
+          <div className="form">
+            <h2>Register</h2>
+            <form onSubmit={handleSubmit}>
+              <div className="inputBox">
+                <input
+                  type="text"
+                  name="fullname"
+                  placeholder="Nombre"
+                  required
+                  value={fullname}
+                  onChange={(e) => setFullname(e.target.value)}
+                />
+              </div>
+              <div className="inputBox">
+                <input
+                  type="email"
+                  name="email"
+                  placeholder="Email"
+                  required
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+              </div>
+              <div className="inputBox">
+                <input
+                  type="text"
+                  name="username"
+                  placeholder="Username"
+                  required
+                  value={username}
+                  onChange={(e) => setUsername(e.target.value)}
+                />
+              </div>
+              <div className="inputBox">
+                <input
+                  type="password"
+                  name="password"
+                  placeholder="Password"
+                  required
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+              </div>
+              <div className="inputBox">
+                <input
+                  type="password"
+                  name="confirmPassword"
+                  placeholder="Confirm Password"
+                  required
+                  value={confirmPassword}
+                  onChange={(e) => setConfirmPassword(e.target.value)}
+                />
+              </div>
+              <div className="inputBox">
+                <input
+                  type="file"
+                  className="form-control"
+                  name="imagen"
+                  accept="image/*"
+                  onChange={(e) => setArchivo(e.target.files[0])}
+                />
+              </div>
+              <div className="inputBox">
+                <input type="submit" value="Registrarse" />
+              </div>
+              <p className="forget">
+                Already have an account? <Link to="/"> Login</Link>
+              </p>
+            </form>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default RegisterForm;
---
+++ readme.txt
@@ -0,0 +1,11 @@
+INTEGRANTES
+--- Mayo
+--- Adriana Mayela
+--- Jordi Alexis Saldaña Ortíz
+2127289 --- María Mercedes Thomas Rivadulla
+
+
+PARA LEVANTAR LA PÁGINA
+1. cd pwii_vbeta
+2. cd front
+3. npm start
